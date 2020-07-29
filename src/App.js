c70a1c955df4ddd021627e77b6b9e39beea11b10
DANIEL COSTA CUNHA <daniel.cunha@itbam.org.br>
2020-07-29 15:11:43 -0400

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Menu from './components/Menu'
-
+import Footer from './components/Footer'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
 
 function App() {
   return (
     <div>
       <Menu />
+
+      <Footer/>
     </div>
   );
 }
